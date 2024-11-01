a7c1e77c6e158baf431a9a8fa78c169f94bb2257
juliet-chime <annychime@gmail.com>
2024-11-01 12:30:52 +0100

    update coin

--- app/api/tickers/route.js
+++ app/api/tickers/route.js
@@ -16,7 +16,14 @@ export async function GET(req, res) {
   const data = await fetch('https://api.coinlore.net/api/tickers/')
   const posts = await data.json()
  
-  return Response.json(posts)
+  return Response.json(posts,{
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
 
   // try{
 
