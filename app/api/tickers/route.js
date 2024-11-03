358c6fc520bac64a6d698c73b36b7d998152bc0d
juliet-chime <annychime@gmail.com>
2024-11-03 10:31:50 +0100

    update coin

--- app/api/tickers/route.js
+++ app/api/tickers/route.js
@@ -13,7 +13,7 @@ export async function GET(req, res) {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Headers": "Content-Type",
     },
   });
 }
